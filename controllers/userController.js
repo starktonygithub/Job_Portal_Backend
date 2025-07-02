8252046f0f712fa75277defa5af3c055b2a1ff84
starktonygitub <pawanyadav35859@gmail.com>
2025-07-02 12:25:21 +0530

    backend project files

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,6 +58,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
